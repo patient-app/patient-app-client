135859dd9dccb7f84ef50396a63e85f5298066f0
Marion Andermatt <marion.andermatt@gmx.ch>
2025-04-02 09:18:23 +0200

    some responsiveness

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,19 +4,20 @@ import ActionProvider from "@/chatbot/ActionProvider";
 import config from "@/chatbot/config";
 import MessageParser from "@/chatbot/MessageParser";
 import Chatbot from "react-chatbot-kit";
+import '../../chatbot/chatbot.css'
 
 export default function ChatPage() {
 
     return (
 
-        <div>
-            <Chatbot
-                config={config}
-                messageParser={MessageParser}
-                actionProvider={ActionProvider}
-                headerText={"I'm a bad Therapis"}
-                placeholderText={"tell me your issues here"}
-            />
+        <div className="flex justify-center">
+                <Chatbot
+                    config={config}
+                    messageParser={MessageParser}
+                    actionProvider={ActionProvider}
+                    headerText={"I'm a bad Therapist"}
+                    placeholderText={"tell me your issues here"}
+                />
         </div>
     );
 
--- src/chatbot/config.tsx
+++ src/chatbot/config.tsx
@@ -6,9 +6,6 @@ const config = {
     initialMessages: [createChatBotMessage(`what up? I'm your ${botName}`, {})],
     botName: botName,
     customStyles: {
-        botMessageBox: {
-            backgroundColor: '#376B7E',
-        },
         chatButton: {
             backgroundColor: '#5ccc9d',
         },
