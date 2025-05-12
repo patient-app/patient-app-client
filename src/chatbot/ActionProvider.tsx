d152cc85a5d3cd24a03d1c867617df82c287a763
Thomas Joos <joos.thomas@outlook.com>
2025-05-12 17:29:03 +0200

    Update ActionProvider.tsx

--- src/chatbot/ActionProvider.tsx
+++ src/chatbot/ActionProvider.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 const ActionProvider = ({ createChatBotMessage, setState, children }: any) => {
     const randomAnswer = () => {
