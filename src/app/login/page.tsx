b789a397465b0bc50e5dac5b319d140822837d99
Marion Andermatt <marion.andermatt@gmx.ch>
2025-05-30 19:37:26 +0200

    #129 adaption to enter email or username to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,7 @@ const Login = () => {
     const {t} = useTranslation();
 
     const [formData, setFormData] = useState<LoginPatientDTO>({
-        email: "",
+        identifier: "",
         password: "",
     });
     const [error, setError] = useState<string | null>(null);
@@ -20,10 +20,11 @@ const Login = () => {
         e.preventDefault();
         setError(null);
 
-        if (!formData.email || !formData.password) {
+        if (!formData.identifier || !formData.password) {
             setError(t("login.error.emptyFields"));
             return;
         }
+        console.log(JSON.stringify(formData))
         try {
             const requestInit: RequestInit = {
                 method: "POST",
@@ -56,14 +57,14 @@ const Login = () => {
             <form onSubmit={handleLogin} className="flex flex-col items-center gap-4 w-full"
                   style={{maxWidth: "20rem"}}>
                 <div className="flex flex-col gap-2 w-full">
-                    <label className="font-semibold" htmlFor="email">{t("login.email")}</label>
+                    <label className="font-semibold" htmlFor="identifier">{t("login.usernameOrEmail")}</label>
                     <input
                         className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-0 focus:border-emerald-600"
-                        name="email"
-                        type="email"
-                        id="email"
-                        placeholder={t("login.email")}
-                        value={formData.email}
+                        name="identifier"
+                        type="text"
+                        id="identifier"
+                        placeholder={t("login.usernameOrEmail")}
+                        value={formData.identifier}
                         onChange={handleChange}
                         required
                     />
@@ -87,8 +88,9 @@ const Login = () => {
                     </div>
 
                     <button
-                        className="w-full mt-3 px-4 py-2 bg-emerald-600 text-white rounded-md hover:bg-emerald-700 transition cursor-pointer"
-                        type="submit" color="primary">
+                        className="w-full mt-3 px-4 py-2 bg-emerald-600 text-white rounded-md hover:bg-emerald-700 transition enabled:cursor-pointer disabled:bg-gray-300"
+                        type="submit"
+                        disabled={!formData.identifier || !formData.password}>
                         {t("login.submit")}
                     </button>
 
