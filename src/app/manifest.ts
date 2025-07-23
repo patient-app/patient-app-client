93528edde329d59452bc83f83909534c97c1e45e
thomi100 <joos.thomas@outlook.com>
2025-07-23 18:07:14 +0200

    updated PWA manifest.ts (#20)

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Next.js PWA',
-        short_name: 'NextPWA',
-        description: 'A Progressive Web App built with Next.js',
+        name: 'Lumina AI',
+        short_name: 'Lumina',
+        description: 'An application to improve mental health.',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
