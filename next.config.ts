271d731606d1d006e6a5ecd7f4b3e9a2ee05bd5d
lukasniedh <146735707+lukasniedh@users.noreply.github.com>
2025-07-24 19:55:17 +0200

    update next.config (#182)

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
-const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const ASSET_PREFIX = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-
-  basePath: BASE_PATH,
-  assetPrefix: BASE_PATH,
+  assetPrefix: ASSET_PREFIX,
 };
 
 export default withFlowbiteReact(nextConfig);
