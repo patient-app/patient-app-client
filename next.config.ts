a9c259450fefcd116885bbb7d26821a3851cccb5
Lukas Niedhart <lukas.niedhart@gmx.ch>
2025-07-24 17:47:07 +0200

    change nextConfig to get basepath

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
+const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
   output: "standalone",
+
+  basePath: BASE_PATH,
+  assetPrefix: BASE_PATH,
 };
 
 export default withFlowbiteReact(nextConfig);
