a312e65a985f60b6b335faa6ed697eeb51c1754b
Marion Andermatt <marion.andermatt@gmx.ch>
2025-07-26 19:16:59 +0200

    fix routing

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const ASSET_PREFIX = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const nextConfig: NextConfig = {
   output: "standalone",
   assetPrefix: ASSET_PREFIX,
+  basePath: "/client",
 };
 
 export default withFlowbiteReact(nextConfig);
--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,3 +1,5 @@
 export const MOBILE_BREAKPOINT = 725;
 
 export const CHATBOT_NAME = "Lumina";
+
+export const BASE_PATH = "/client";
